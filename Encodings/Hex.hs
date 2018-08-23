02028327bcc973eca5429ad7b0c21fcd2a199cbb
George Karachalias <george.karachalias@gmail.com>
2018-08-23 11:16:26 +0200

    Encodings.Hex: Add comments and stdout/stdin utilities

--- Encodings/Hex.hs
+++ Encodings/Hex.hs
@@ -1,6 +1,10 @@
 {-# OPTIONS_GHC -Wall #-}
 
-module Encodings.Hex (Hex(..), showHexBytes, readHexDigit, readHexBytes) where
+module Encodings.Hex
+( Hex(..)
+, showHexBytes, readHexDigit, readHexBytes
+, getHexLine, getHexWords, getHexLines, printHex
+) where
 {-
   Convert from numeric types to hex strings and vice versa.
 -}
@@ -12,7 +16,7 @@ import Data.List (foldl')
 import Data.Words
 import Utils.Utils
 
--- * Class for things that can be printed to and read as hexadecimal
+-- * Class for things that can be printed to and read as hexadecimal strings
 -- ----------------------------------------------------------------------------
 
 class Hex a where
@@ -58,17 +62,20 @@ instance Hex BS.ByteString where
 -- * Hexadecimal printing utilities
 -- ----------------------------------------------------------------------------
 
--- | Show in hexadecimal anything that consists of bytes
+-- | Show in hexadecimal anything that consists of bytes (can be used as the
+-- default implementation of 'showHex' if the type is also an instance of
+-- 'Bytes')
 showHexBytes :: Bytes a => a -> String
 showHexBytes = concatMap showHexWord8 . toBytes
 
--- | Show a single byte in hexadecimal (2 hex digits)
+-- | Show a single byte in hexadecimal (two hex digits)
 showHexWord8 :: Word8 -> String
 showHexWord8 w = replicate (2 - length h) '0' ++ h where h = N.showHex w ""
 
 -- * Hexadecimal parsing utilities
 -- ----------------------------------------------------------------------------
 
+-- | Turn a hexadecimal string into a number
 readHexBytes :: (Num a, Bytes a) => String -> a
 readHexBytes str
   | noBytes result * 2 == length str = result -- each byte corresponds to two hex digits
@@ -77,6 +84,7 @@ readHexBytes str
     result = fromInteger
            $ foldl' (\acc x -> 16*acc + readHexDigit x) 0 str
 
+-- | Turn a hexadecimal string into a bytestring
 readHexByteString :: String -> BS.ByteString
 readHexByteString str
   | Just ps <- pair str = fromBytes (map fn ps)
@@ -84,6 +92,7 @@ readHexByteString str
   where
     fn (c1,c2) = (readHexDigit c1 `rotateL` 4) .|. readHexDigit c2
 
+-- | Turn a hexadecimal character into a number
 readHexDigit :: Num a => Char -> a
 readHexDigit c
   | ordc >= 48, ordc <= 57  {- 0..9 -} = fromIntegral (ordc - 48)
@@ -92,11 +101,24 @@ readHexDigit c
   | otherwise = error $ "readHexDigit: " ++ c : " is not in not a hex char"
   where
     ordc = ord c
+    -- ord 'A' -> 65, 'F' -> 70, 'a' -> 97, 'f' -> 102, '0' -> 48, '9' -> 57
 
-    -- ord 'A' -> 65
-    -- ord 'F' -> 70
-    -- ord 'a' -> 97
-    -- ord 'f' -> 102
-    -- ord '0' -> 48
-    -- ord '9' -> 57
+-- * Hexadecimal strings input / output
+-- ----------------------------------------------------------------------------
+
+-- | Parse a line as a hexadecimal
+getHexLine :: Hex a => IO a
+getHexLine = readHex <$> getLine
+
+-- | Parse a line as a space-separated hexadecimals
+getHexWords :: Hex a => IO [a]
+getHexWords = map readHex . words <$> getLine
+
+-- | Parse a file as a newline-separated hexadecimals
+getHexLines :: Hex a => IO [a]
+getHexLines = map readHex . lines <$> getContents
+
+-- | Print something in hexadecimal and change line
+printHex :: Hex a => a -> IO ()
+printHex = putStrLn . showHex
 
