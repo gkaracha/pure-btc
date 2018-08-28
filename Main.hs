ff4a882987d204e6badf8102ed563cc298c071bc
George Karachalias <george.karachalias@gmail.com>
2018-08-28 18:49:30 +0200

    wibbles

--- Main.hs
+++ Main.hs
@@ -2,30 +2,30 @@
 
 module Main where
 
-import Data.Word.Word128 ()
-import Data.Word.Word160 ()
-import Data.Word.Word256 ()
-import Data.Word.Word512 ()
-import Data.Words         ()
+import Data.Word.Partition  ()
+import Data.Word.Word128    ()
+import Data.Word.Word160    ()
+import Data.Word.Word256    ()
+import Data.Word.Word512    ()
 
-import Encoding.Hex         ()
-import Encoding.Base58      ()
 import Encoding.Base58Check ()
+import Encoding.Base58      ()
+import Encoding.Hex         ()
 import Encoding.WIF         ()
 
-import Util.ByteString ()
-import Util.Error      ()
-import Util.List       ()
-import Util.Numeric    ()
+import Hash.ECM             ()
+import Hash.HASH160         ()
+import Hash.RIPEMD160       ()
+import Hash.SHA256          ()
 
-import Hash.SHA256    ()
-import Hash.RIPEMD160 ()
-import Hash.ECM       ()
-import Hash.HASH160   ()
+import Key.Address          ()
+import Key.Private          ()
+import Key.Public           ()
 
-import Key.Private ()
-import Key.Public  ()
-import Key.Address ()
+import Util.ByteString      ()
+import Util.Error           ()
+import Util.List            ()
+import Util.Numeric         ()
 
 main :: IO ()
 main = putStrLn "Remember to load all files to make sure they compile!"
